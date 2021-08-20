3b63038b70e6b572d3dee8ba249497b52178ea12
Jeremy F McKay <jeremyfmckay@gmail.com>
2021-08-20 16:07:05 -0700

    finished nav need to set up for my use.

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,24 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Me',
+        url: '#',
+        cName: 'nav-links'
+    },    {
+        title: 'Projects',
+        url: '#',
+        cName: 'nav-links'
+    },    {
+        title: 'Contact-Me',
+        url: '#',
+        cName: 'nav-links'
+    },    {
+        title: 'Sign-up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
